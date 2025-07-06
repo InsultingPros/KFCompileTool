e57591ac8a6bdc7d86f22a732a342ff4baee758d
Shtoyan <shtoyan@proton.me>
2025-07-06 19:35:39 +0400

    a

--- src/config_manager/steam_appid.rs
+++ src/config_manager/steam_appid.rs
@@ -1,9 +1,15 @@
 use crate::{RuntimeVariables, errors::CompileToolErrors};
 use std::{
-    fs::{self, OpenOptions, Permissions},
+    fs::{self, OpenOptions},
     io::Write as _,
     path::PathBuf,
 };
+
+#[derive(Debug)]
+enum FileEditPermission {
+    ReadOnly,
+    Write,
+}
 
 /// Included default config file.
 pub const STEAM_APPID_TXT: &str = "steam_appid.txt";
@@ -16,7 +22,7 @@ pub fn create_hacky_steamappid(runtime_vars: &RuntimeVariables) -> Result<(), Co
     // remove the file if it exists
     if steam_appid_file.try_exists()? {
         // Set file attributes to normal (removing read-only)
-        set_steamappid_readonly(steam_appid_file, false)?;
+        set_steamappid_readonly(steam_appid_file, &FileEditPermission::Write)?;
         // Now delete the file
         fs::remove_file(steam_appid_file)?;
     }
@@ -28,18 +34,38 @@ pub fn create_hacky_steamappid(runtime_vars: &RuntimeVariables) -> Result<(), Co
         .truncate(true)
         .open(steam_appid_file)?;
     hack_file.write_all(b"3")?;
-    set_steamappid_readonly(steam_appid_file, true)?;
+    set_steamappid_readonly(steam_appid_file, &FileEditPermission::ReadOnly)?;
 
     Ok(())
 }
 
 /// Set file readonly attribute
-/// # Errors
-/// _
-pub fn set_steamappid_readonly(file: &PathBuf, to: bool) -> Result<(), CompileToolErrors> {
-    let mut perms: Permissions = fs::metadata(file)?.permissions();
-    perms.set_readonly(to);
-    fs::set_permissions(file, perms)?;
+fn set_steamappid_readonly(
+    file: &PathBuf,
+    to: &FileEditPermission,
+) -> Result<(), CompileToolErrors> {
+    let metadata = match fs::metadata(file) {
+        Ok(result) => result,
+        Err(e) => {
+            eprintln!("Failed to retrieve metadata from file {}", file.display());
+            return Err(CompileToolErrors::IOError(e));
+        }
+    };
+    let mut permsissions = metadata.permissions();
+
+    let b: bool = match to {
+        FileEditPermission::ReadOnly => true,
+        FileEditPermission::Write => false,
+    };
+    permsissions.set_readonly(b);
+
+    if let Err(e) = fs::set_permissions(file, permsissions) {
+        eprintln!(
+            "Failed to set {to:?} permission for file {}",
+            file.display()
+        );
+        return Err(CompileToolErrors::IOError(e));
+    }
 
     Ok(())
 }
